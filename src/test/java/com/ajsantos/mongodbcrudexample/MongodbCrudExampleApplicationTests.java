0bc13a8bbcff7af7e3107bbccd539a62c78fe410
Alan Santos <alanjosedossantos011@gmail.com>
2022-03-22 17:33:19 -0300

    commit incial

---
+++ src/test/java/com/ajsantos/mongodbcrudexample/MongodbCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajsantos.mongodbcrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
